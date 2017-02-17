89fe0073e9625825b6fcb36c55d72659882c4de9
alfran <francesco@arduino.org>
2017-02-17 14:39:52 +0100

    updated SPI.cpp and SPI.h

--- cores/arduino/SPI.h
+++ cores/arduino/SPI.h
@@ -15,7 +15,6 @@
 #define _SPI_H_INCLUDED
 
 #include <Arduino.h>
-extern SPI_HandleTypeDef hSPIx;
 
 #define SPIx                             HAL_SPI2
 #define SPIx_CLK_ENABLE()                __HAL_RCC_SPI2_CLK_ENABLE()
@@ -68,55 +67,73 @@ extern SPI_HandleTypeDef hSPIx;
 #define SPI_MODE2 0x08
 #define SPI_MODE3 0x0C
 
-#define SPI_LAST  0
-#define SPI_CONTINUE 1
+enum SPITransferMode {
+	SPI_CONTINUE,
+	SPI_LAST
+};
 
 
 class SPISettings {
 public:
-  SPISettings(uint32_t clock, uint8_t bitOrder, uint8_t dataMode) {
-      init_AlwaysInline(clock, bitOrder, dataMode);
+  SPISettings(uint32_t clock, uint8_t bitOrder, uint8_t dataMode)
+  {
+    init_AlwaysInline(clock, bitOrder, dataMode);
   }
-  SPISettings() {
-    init_AlwaysInline(SPI_CLOCK_DIV4, MSBFIRST, SPI_MODE0);
+  SPISettings()
+  {
+    init_AlwaysInline(4000000, MSBFIRST, SPI_MODE0);
   }
 private:
   void init_AlwaysInline(uint32_t clock, uint8_t bitOrder, uint8_t dataMode)
     __attribute__((__always_inline__)) {
 
     /* Select the Bit Order */
-    if(bitOrder == LSBFIRST) {
-      bitOrder = SPI_FIRSTBIT_LSB;
+    if(bitOrder == LSBFIRST)
+    {
+      FirstBit = SPI_FIRSTBIT_LSB;
     }
-    else {
-      bitOrder = SPI_FIRSTBIT_MSB;
+    else
+    {
+      FirstBit = SPI_FIRSTBIT_MSB;
     }
-    hSPIx.Init.FirstBit = bitOrder;
 
     /* Select the SPI Communication Mode */
-    if(dataMode == SPI_MODE0) {
-      hSPIx.Init.CLKPhase          = SPI_PHASE_1EDGE;
-      hSPIx.Init.CLKPolarity       = SPI_POLARITY_LOW;
+    if(dataMode == SPI_MODE0)
+    {
+      CLKPhase          = SPI_PHASE_1EDGE;
+      CLKPolarity       = SPI_POLARITY_LOW;
     }
-    else if(dataMode == SPI_MODE1) {
-      hSPIx.Init.CLKPhase          = SPI_PHASE_2EDGE;
-      hSPIx.Init.CLKPolarity       = SPI_POLARITY_LOW;
+    else if(dataMode == SPI_MODE1)
+    {
+      CLKPhase          = SPI_PHASE_2EDGE;
+      CLKPolarity       = SPI_POLARITY_LOW;
     }
-    else if(dataMode == SPI_MODE2) {
-      hSPIx.Init.CLKPhase          = SPI_PHASE_1EDGE;
-      hSPIx.Init.CLKPolarity       = SPI_POLARITY_HIGH;
+    else if(dataMode == SPI_MODE2)
+    {
+      CLKPhase          = SPI_PHASE_1EDGE;
+      CLKPolarity       = SPI_POLARITY_HIGH;
     }
-    else {
-      hSPIx.Init.CLKPhase          = SPI_PHASE_2EDGE;
-      hSPIx.Init.CLKPolarity       = SPI_POLARITY_HIGH;
+    else
+    {
+      CLKPhase          = SPI_PHASE_2EDGE;
+      CLKPolarity       = SPI_POLARITY_HIGH;
     }
 
     /* Select the Clock Divider */
-    hSPIx.Init.BaudRatePrescaler = clock;
-
-    /* Initialize the SPIx */
-    HAL_SPI_Init(&hSPIx);
+    BaudRatePrescaler = (((HAL_RCC_GetPCLK1Freq()) / 2  ) < clock) ? SPI_CLOCK_DIV2
+                      : (((HAL_RCC_GetPCLK1Freq()) / 4  ) < clock) ? SPI_CLOCK_DIV4
+                      : (((HAL_RCC_GetPCLK1Freq()) / 8  ) < clock) ? SPI_CLOCK_DIV8
+                      : (((HAL_RCC_GetPCLK1Freq()) / 16 ) < clock) ? SPI_CLOCK_DIV16
+                      : (((HAL_RCC_GetPCLK1Freq()) / 32 ) < clock) ? SPI_CLOCK_DIV32
+                      : (((HAL_RCC_GetPCLK1Freq()) / 64 ) < clock) ? SPI_CLOCK_DIV64
+                      : (((HAL_RCC_GetPCLK1Freq()) / 128) < clock) ? SPI_CLOCK_DIV128
+                      :                                              SPI_CLOCK_DIV256;
   }
+
+  uint32_t FirstBit;
+  uint32_t CLKPhase;
+  uint32_t CLKPolarity;
+  uint32_t BaudRatePrescaler;
 
   friend class SPIClass;
 };
@@ -124,37 +141,43 @@ private:
 
 class SPIClass {
 public:
+  /* Constructor */
+  SPIClass(SPI_TypeDef *spiInstance);
   /* Initialize the SPI peripheral */
-  static void begin();
+  void begin();
 
   /* Initialize the SPI peripheral and SS pin */
-  static void begin(uint8_t slaveSelectPin);
+  void begin(uint8_t slaveSelectPin);
 
   /* Initialize the SPI peripheral with settings */
-  static void beginTransaction(SPISettings settings);
+  void beginTransaction(SPISettings settings);
 
   /* Initialize the SPI peripheral */
-  static void endTransaction();
+  void endTransaction();
 
   /* Begin the transfer */
-  static uint8_t transfer(uint8_t data);
-  static uint16_t transfer16(uint16_t data);
-  static void transfer(void *buf, size_t count);
-  static void transfer(uint8_t slaveSelectPin, uint8_t val, uint8_t transferMode);
+  uint8_t transfer(uint8_t data);
+  uint16_t transfer16(uint16_t data);
+  void transfer(void *buf, size_t count);
+  uint8_t transfer(uint8_t slaveSelectPin, uint8_t val, SPITransferMode transferMode);
 
   /* End the transfer */
-  static void end();
+  void end();
 
-  static void end(uint8_t slaveSelectPin);
+  void end(uint8_t slaveSelectPin);
 
   /* Set Bit Order */
-  static void setBitOrder(uint8_t bitOrder);
+  void setBitOrder(uint8_t bitOrder);
 
   /* Set Clock Divider */
-  static void setClockDivider(uint8_t clockDiv);
+  void setClockDivider(uint8_t clockDiv);
 
   /* Set Communication Mode */
-  static void setDataMode(uint8_t dataMode);
+  void setDataMode(uint8_t dataMode);
+
+private:
+
+  SPI_HandleTypeDef hSPIx;
 
 };
 
