64ab58d0e376ea721876921c629f4c1b7afee049
alfran <francesco@arduino.org>
2017-03-03 12:15:00 +0100

    improved compatibility

--- cores/arduino/io.h
+++ cores/arduino/io.h
@@ -43,72 +43,16 @@
 #include "adc.h"
 
 #include "wiring_time.h"
-
-/**
- * Specifies a GPIO pin behavior.
- * @see pinMode()
- */
-typedef enum WiringPinMode {
-    INPUT, /**< Basic digital input. The pin voltage is sampled; when
-              it is closer to 3.3v (Vcc) the pin status is high, and
-              when it is closer to 0v (ground) it is low. If no
-              external circuit is pulling the pin voltage to high or
-              low, it will tend to randomly oscillate and be very
-              sensitive to noise (e.g., a breath of air across the pin
-              might cause the state to flip). */
-              
-    OUTPUT, /**< Basic digital output: when the pin is HIGH, the
-               voltage is held at +3.3v (Vcc) and when it is LOW, it
-               is pulled down to ground. */
-
-    OUTPUT_OPENDRAIN, /**< In open drain mode, the pin indicates
-                          "low" by accepting current flow to ground
-                          and "high" by providing increased
-                          impedance. An example use would be to
-                          connect a pin to a bus line (which is pulled
-                          up to a positive voltage by a separate
-                          supply through a large resistor). When the
-                          pin is high, not much current flows through
-                          to ground and the line stays at positive
-                          voltage; when the pin is low, the bus
-                          "drains" to ground with a small amount of
-                          current constantly flowing through the large
-                          resistor from the external supply. In this
-                          mode, no current is ever actually sourced
-                          from the pin. */
-
-
-
-    INPUT_PULLUP, /**< The state of the pin in this mode is reported
-                     the same way as with INPUT, but the pin voltage
-                     is gently "pulled up" towards +3.3v. This means
-                     the state will be high unless an external device
-                     is specifically pulling the pin down to ground,
-                     in which case the "gentle" pull-up will not
-                     affect the state of the input. */
-
-    INPUT_PULLDOWN, /**< The state of the pin in this mode is reported
-                       the same way as with INPUT, but the pin voltage
-                       is gently "pulled down" towards 0v. This means
-                       the state will be low unless an external device
-                       is specifically pulling the pin up to 3.3v, in
-                       which case the "gentle" pull-down will not
-                       affect the state of the input. */
-
-    INPUT_FLOAT, /**< Synonym for INPUT. */
-
-    OUTPUT_DAC, /**< This is a special mode for when the pin will be used for
-                     PWM output (a special case of digital output). */
-} WiringPinMode;
+#include "wiring_constants.h"
 
 /**
  * Configure behavior of a GPIO pin.
  *
  * @param pin Number of pin to configure.
  * @param mode Mode corresponding to desired pin behavior.
- * @see WiringPinMode
+ * @see PinMode
  */
-void pinMode(uint8 pin, WiringPinMode mode);
+void pinMode(uint8 pin, uint8 mode);
 
 /**
  * Writes a (digital) value to a pin.  The pin must have its
