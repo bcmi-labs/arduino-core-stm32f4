4cea4f14e56b91acf52514aaaa85957fdbc97d9f
Arturo Rinaldi <arty.net2@gmail.com>
2017-03-03 14:38:35 +0100

    updating post_install.bat

--- post_install.bat
+++ post_install.bat
@@ -2,12 +2,9 @@
 set ARGS=/A /SE /SW /SA
 if "%PROCESSOR_ARCHITECTURE%" == "AMD64" (
   drivers\dpinst-amd64.exe %ARGS%
-  drivers\dpinst-amd64.exe %ARGS% /path %CD%\drivers\x64
 ) ELSE IF "%PROCESSOR_ARCHITEW6432%" == "AMD64" (
   drivers\dpinst-amd64.exe %ARGS%
-  drivers\dpinst-amd64.exe %ARGS% /path %CD%\drivers\x64
 ) ELSE (
   drivers\dpinst-x86.exe %ARGS%
-  drivers\dpinst-x86.exe %ARGS% /path %CD%\drivers\x86
 )
 exit /b 0
